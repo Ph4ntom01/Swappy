cbbc3a8643201ad29b316f693de02919d9611a48
b4rcode@pm.me <b4rcode@pm.me>
2022-10-22 17:32:10 +0200

    Update Swappy version

--- src/main/java/swappy/EConstant.java
+++ src/main/java/swappy/EConstant.java
@@ -2,7 +2,7 @@ package swappy;
 
 public enum EConstant {
 
-    SWAPPY_VERSION("1.1"), FRAME_BORDER_DARK("#F0E68C"), FRAME_BORDER_LIGHT("#6290F4"), BORDER_THICKNESS("3");
+    SWAPPY_VERSION("1.2"), FRAME_BORDER_DARK("#F0E68C"), FRAME_BORDER_LIGHT("#6290F4"), BORDER_THICKNESS("3");
 
     private String constant;
 
